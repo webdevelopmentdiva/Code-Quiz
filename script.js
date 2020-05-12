1b45d56300914ce8608f13a2ce99da91fa37b8ed
Skyler Bluth <webdevelopmentdiva@gmail.com>
2020-05-12 16:35:10 -0700

    rewritten code for hw

--- script.js
+++ script.js
@@ -1,115 +1,100 @@
+function buildQuiz(){}
+function showResults(){}
+
 // When user loads the page, they see title, description,and a start quiz button
 // They're going to click "start quiz"
-document.getElementById("quizStart").addEventListener("click", quiz);
+const quizContainer = document.getElementById("quiz");
+const resultsContainer = document.getElementById("results");
+const submitButton = document.getElementById("submit");
 
 
-//Once we click start, display questions, choice of answers, start timer
-function nextQuestion() {
-    
-    
-}
-
-        function quiz() {
-            questions(questions, document.getElementById("#quizContainer"));
-        console.log(quizTimer);
+//displaying quiz questions
+    const myQuestions = [
+        {
+            question: "1. What year did I graduate?",
+            answers: {
+                a: "2019", b: "2011", c: "2013", d: "2015",
+            },
+            correctAnswer: "c"
+        },
+        {
+            question: "2. Which unnatural hair color did I have?",
+            answers: {
+                a: "blue", b: "purple", c:"orange", d: "pink",
+            },
+            correctAnswer: "b"
+        },
+        {
+            question: "3. Would I rather travel or dine at the fanciest restaurants?",
+            answers: {
+                a: "travel", b: "dine like a queen",
+            },
+            correctAnswer: "a"
+        },
+        {
+            question: "4. What camera do I shoot with?",
+            answers: {
+                a: "Canon", b: "Sony", c: "Nikon",
+            },
+            correctAnswer: "b"
+        },
+        {
+            question: "5. Do I prefer cats or dogs?",
+            answers: {
+                a: "dogs", b: "cats",
+            },
+            correctAnswer: "b"
         }
-        var time = 90 ;
-        function quizTimer() {
-            var timeLeft = 90;
-            var timeInterval = setInterval(function () {
-              timerEl.textContent = timeLeft + " seconds remaining";
-              timeLeft--;
-              if (timeLeft === 0) {
-                timerEl.textContent = "";
-                speedRead();
-                clearInterval(timeInterval);
-              }
-            }, 1000);
-        }
+    ];
 
-var questions = [
-    {
-        q1: "1. What year did I graduate?",
-        answer1: {
-            a: "2019", b: "2011", c: "2013", d: "2015",
-        },
-        correctAnswer: "2013"
-    },
-    {
-        q2: "2. Which unnatural hair color did I have?",
-        answer2: {
-            a: "blue", b: "purple", c:"orange", d: "pink",
-        },
-        correctAnswer: "purple"
-    },
-    {
-        q3: "3. Would I rather travel or dine at the fanciest restaurants?",
-        answer2: {
-            a: "travel", b: "dine like a queen",
-        },
-        correctAnswer: "travel"
-    },
-    {
-        q4: "4. What camera do I shoot with?",
-        answer2: {
-            a: "Canon", b: "Sony", c: "Nikon",
-        },
-        correctAnswer: "Sony"
-    },
-    {
-        q5: "5. Do I prefer cats or dogs?",
-        answer2: {
-            a: "dogs", b: "cats",
-        },
-        correctAnswer: "cats"
+    
+
+//display quiz right away
+buildQuiz();
+
+//on Submit, show results
+submitButton.addEventListener("click", showResults);
+
+
+    //display the list of questions on the page
+    function buildQuiz(){
+        // variable to store the HTML output
+        const output = [];
+
+        // for each question 
+        myQuestions.forEach(
+            (currentQuestion, questionNumber) => {
+
+                //variable to store the list of possible answers
+                const answers = [];
+
+                //and for each available answer
+                for(letter in currentQuestion.answers){
+
+                    //add an HTML radio button
+                    answers.push(
+                        <label>
+                            <input type="radio" name="question"${questionNumber}" value="${letter}"/>
+                            ${letter} :
+                            ${currentQuestion.answers[letter]}
+                            </label>
+                    );
+                }
+                // add this question and its answers to the output
+                output.push(
+                    <div class="question">${currentQuestion.question}</div>
+                    <div class="answers">${answers.join("")}</div>
+                );
+            }
+        );
+
+        //finally combine our output list into one string of HTML and put it on the page
+        quizContainer.innerHTML = output.join("");
     }
-]
 
-//keep track of the score of the user
-var currentScore = 0;
-var correctAnswers = 0;
-var incorrectAnswers = 0;
-var unanswered = 0;
+    //start building the HTML for each question
+    // we'll need to loops through each question
 
-
-questionIndex = 0;// Tucker had these???
-var numCorrect = 0;
-var time = 100;
-var $timer = documentSelector("#timer");
-var $startBtn = document.querySelector("#startBtn");
-var $ = documentSelector("#");//not sure if I need these
-
-
-
-
-//if user checks correct box, increase correct by 1++  
-//function to display the next question
-
-function nextQuestion
-for() {
-    if (correct === correctAnswer) {
-        correctAnswers ++;
-        nextQuestion();
-    }
-}
-else if (correct != correctAnswer) {
-    incorrectAnswer++;
-    nextQuestion()
-} 
-
-
-//if user chooses incorrect box, incorrect by 1++
-//function to display the next question
-
-function nextQuestion
-
-
-
-// if user runs out of time unanswered, increase by 1++
-if
-//function to display the next question
-function nextQuestion
-
-
-
-//Display Results
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+        //the code we want to run for each question goes here
+    });
